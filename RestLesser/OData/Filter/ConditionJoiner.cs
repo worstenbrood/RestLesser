f03bcc3cdb1e36534b7013689180f02bca93903d
Tim Horemans <tim.horemans@tenforce.com>
2025-11-16 23:56:26 +0100

    Use primary constructor

--- RestLesser/OData/Filter/ConditionJoiner.cs
+++ RestLesser/OData/Filter/ConditionJoiner.cs
@@ -6,15 +6,12 @@ namespace RestLesser.OData.Filter
     /// <summary>
     /// Class that joins multiple conditions
     /// </summary>
-    public class ConditionJoiner : ConditionBase<string>
-    {
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="conditions"></param>
-        public ConditionJoiner(params ICondition[] conditions) : base(string.Join(Constants.Query.ConditionSeparator, 
+    /// <remarks>
+    /// Constructor
+    /// </remarks>
+    /// <param name="conditions"></param>
+    public class ConditionJoiner(params ICondition[] conditions) : ConditionBase<string>(string.Join(Constants.Query.ConditionSeparator, 
             conditions.Select(c => c.ToString())))
-        {
-        }
+    {
     }
 }
