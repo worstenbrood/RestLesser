eb784a568341162af4cc68d216db6acbc98f85d1
Tim Horemans <tim.horemans@tenforce.com>
2025-11-18 10:18:54 +0100

    Added TvMaze example

---
+++ RestLesser.Examples/TvMaze/Models/Show.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace RestLesser.Examples.TvMaze.Models
+{
+    // https://www.tvmaze.com/api#show-main-information
+
+    public class Show
+    {
+        [JsonProperty("id")]
+        public int? Id { get; set; }
+
+        [JsonProperty("url")]
+        public string? Url { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
+
+        [JsonProperty("type")]
+        public string? Type { get; set; }
+
+        [JsonProperty("language")]
+        public string? Language { get; set; }
+    }
+}
---
+++ RestLesser.Examples/TvMaze/TvMazeClient.cs
@@ -0,0 +1,16 @@
+﻿using RestLesser.Examples.TvMaze.Models;
+
+namespace RestLesser.Examples.TvMaze
+{
+    public class TvMazeClient : RestClient
+    {
+        public TvMazeClient() : base("https://api.tvmaze.com")
+        { 
+        }
+
+        public Show GetShow(int id)
+        {
+            return Get<Show>($"/shows/{id}");
+        }
+    }
+}
