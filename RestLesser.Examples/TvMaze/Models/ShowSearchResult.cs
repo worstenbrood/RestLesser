546fda08f6ac9c9c5cc9a34082e4df1ed728af81
Tim Horemans <tim.horemans@tenforce.com>
2025-11-22 15:29:45 +0100

    Added JsonProperty

--- RestLesser.Examples/TvMaze/Models/ShowSearchResult.cs
+++ RestLesser.Examples/TvMaze/Models/ShowSearchResult.cs
@@ -1,9 +1,13 @@
-﻿namespace RestLesser.Examples.TvMaze.Models
+﻿using Newtonsoft.Json;
+
+namespace RestLesser.Examples.TvMaze.Models
 {
     public class ShowSearchResult
     {
+        [JsonProperty("score")]
         public decimal? Score { get; set; }
 
+        [JsonProperty("show")]
         public Show? Show { get; set; }
     }
 }
