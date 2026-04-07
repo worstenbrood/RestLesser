0755a0612defe44e8fd2ed28db7ed345710a37df
Tim Horemans <tim.horemans@tenforce.com>
2026-04-07 10:14:25 +0200

    Cleanup

--- Restlesser.Builder/Parsers/NameParser.cs
+++ Restlesser.Builder/Parsers/NameParser.cs
@@ -2,23 +2,25 @@
 {
     internal class NameParser
     {
+        public const char Separator = '.';
+
         public readonly string[] Parts;
         public readonly string Name;
         public readonly string FullName;
         public readonly string Namespace;
         public readonly string Path;
         
-        public static string GetNamespace(string fullName) => string.Join('.', fullName.Split('.').Reverse().Skip(1).Reverse());
+        public static string GetNamespace(string fullName) => string.Join(Separator, fullName.Split(Separator).Reverse().Skip(1).Reverse());
 
-        public static string GetName(string fullName) => fullName.Split('.').Last();
+        public static string GetName(string fullName) => fullName.Split(Separator).Last();
 
         public NameParser(string fullName)
         {
-            Parts = fullName.Split('.');
+            Parts = fullName.Split(Separator);
             FullName = fullName;
             Name = Parts.Last();
-            Namespace = string.Join('.', Parts[..^1]);
-            Path = System.IO.Path.Combine([.. Parts.Take(Parts.Length - 1)]);
+            Namespace = string.Join(Separator, Parts[..^1]);
+            Path = System.IO.Path.Combine(Parts[..^1]);
         }
     }
 }
